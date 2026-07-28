b60a648ef74b4b401306f81e311540637e3ee9ba
noahkoldow <n.l.kothlow@gmail.com>
2026-07-28 09:02:13 +0200

    fix(firebase): correctly initialize and export firebase services

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
@@ -13,6 +15,14 @@ export const firebaseConfig = {
   measurementId: "G-DF02468X9W"
 };
 
+const hasRequiredFirebaseConfig = Boolean(
+  firebaseConfig.apiKey
+    && firebaseConfig.projectId
+    && firebaseConfig.appId
+);
+export const firebaseEnabled = hasRequiredFirebaseConfig;
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
